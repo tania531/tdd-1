d8cf9bf50ba566b57f889910ebf7097ed82ad7b3
Tania <tania.dev77@gmail.com>
2015-06-11 15:52:58 -0700

    first two doubler test functions

---
+++ index3.js
@@ -0,0 +1,36 @@
+var doubler = function(a, b) {
+  if(typeOf(a)  === 'string' && typeOf(b)  === 'string'){
+    return (a+a) + (b+b);
+  } else if (typeOf(a) === 'number' && typeOf(b) === 'number'){
+    return a*2 + b*2;
+  }
+
+};
+
+
+console.assert(doubler(4,2) === 12);
+console.assert(doubler(1,3) === 8);
+console.assert(doubler(-1,3) === 4);
+
+console.assert(doubler("a", "b") === "aabb");
+console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
+
+
+
+function m3() { return 3; }
+function m4() { return 4; }
+function m2() { return 2; }
+function m1() { return 1; }
+
+console.assert(doubler(m4, m2) === 12);
+console.assert(doubler(m1, m3) === 8);
+
+function objectsEqual() { }
+
+var a = { z: 42 , t: 7 }, b = { t: 7, z: 42 }
+
+console.assert(objectsEqual(a, b));
+console.assert(objectsEqual(b, a));
+
+objectsEqual(doubler({x1: 1, x2: 4}), {x1: 2, x2: 8});
+objectsEqual(doubler({x1: -4, x2: 0}), {x1: -8, x2: 0});
