82f6d5912dbf8b0ce6a889de5cdb8a64cf7c8837
AgileLabs <samer@agilelabs.com>
2015-06-11 15:06:22 -0700

    TDD, take 2

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ var doubler = function(a, b) {
 console.assert(doubler(4,2) === 12);
 console.assert(doubler(1,3) === 8);
 console.assert(doubler(-1,3) === 4);
+
+console.assert(doubler("a", "b") === "aabb");
+console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
