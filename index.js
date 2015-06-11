2ed9a3b54ce70468aff1f7f9e8cff60f79862850
AgileLabs <samer@agilelabs.com>
2015-06-11 15:19:12 -0700

    TDD, take 3

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ console.assert(doubler(-1,3) === 4);
 
 console.assert(doubler("a", "b") === "aabb");
 console.assert(doubler("coding", "rocks") === "codingcodingrocksrocks");
+
+function m3() { return 3; }
+function m4() { return 4; }
+function m2() { return 2; }
+function m1() { return 1; }
+
+console.assert(doubler(m4, m2) === 12);
+console.assert(doubler(m1, m3) === 8);
